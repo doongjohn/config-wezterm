aa566254b950d749344e0bdc567beb789c582032
doongjohn <40219740+doongjohn@users.noreply.github.com>
2022-04-26 15:10:09 +0900

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -19,30 +19,35 @@ return {
 
   -- font settings
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- disable ligatures
-  line_height = 1.3,
-  font_size = 14.1,
+  line_height = 1.34,
+  font_size = 14,
   font = wezterm.font_with_fallback({
     'VictorMono NF', -- nerd font
     'NanumGothicCoding', -- korean font
+    'Cambria Math', -- NOTE: some math symbols mess up neovim...
+    'Segoe UI Emoji',
   }),
+  adjust_window_size_when_changing_font_size = false,
 
   -- disable hyperlink
   hyperlink_rules = {{ regex = '', format = '', }},
 
   -- window
+  window_decorations = "RESIZE",
+  initial_cols = 120,
+  initial_rows = 23,
   window_frame = {
     font_size = 10.0,
   },
   window_padding = {
+    top = 4,
+    bottom = 0,
     left = 12,
     right = 12,
-    top = 12,
-    bottom = 0
   },
 
   -- tab bar settings
   use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
 
   -- kanagawa color scheme
   colors = {
@@ -79,8 +84,29 @@ return {
     selection_bg = '#2d4f67',
     scrollbar_thumb = '#16161d',
     split = '#16161d',
-    ansi = { '#090618', '#c34043', '#76946a', '#c0a36e', '#7e9cd8', '#957fb8', '#6a9589', '#c8c093' },
-    brights = { '#727169', '#e82424', '#98bb6c', '#e6c384', '#7fb4ca', '#938aa9', '#7aa89f', '#dcd7ba' },
-    indexed = { [16] = '#ffa066', [17] = '#ff5d62' },
+    ansi = {
+      '#090618',
+      '#c34043',
+      '#76946a',
+      '#c0a36e',
+      '#7e9cd8',
+      '#957fb8',
+      '#6a9589',
+      '#c8c093'
+    },
+    brights = {
+      '#727169',
+      '#e82424',
+      '#98bb6c',
+      '#e6c384',
+      '#7fb4ca',
+      '#938aa9',
+      '#7aa89f',
+      '#dcd7ba'
+    },
+    indexed = {
+      [16] = '#ffa066',
+      [17] = '#ff5d62'
+    },
   },
 }
